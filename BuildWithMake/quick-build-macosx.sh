a65eb5c11761e20807a85d223b5e809fa1b7fc91
osmsc <nwilson@osmsc.com>
2019-11-16 11:46:56 -0800

    BUILD: adding quick build scripts for make build just like SV

---
+++ BuildWithMake/quick-build-macosx.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Copyright (c) Stanford University, The Regents of the University of
+#               California, and others.
+#
+# All Rights Reserved.
+#
+# See Copyright-SimVascular.txt for additional details.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject
+# to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+# OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#export SV_EXTERNALS_VERSION_NUMBER=2019.02
+export SV_EXTERNALS_VERSION_NUMBER=2019.06
+
+export EXTERNALS_SV_ARCH_DIR=x64
+export EXTERNALS_SV_OS_DIR=mac_osx
+export EXTERNALS_SV_OS_LONG_NAME_DIR=mac_osx
+export EXTERNALS_SV_COMPILER_SHORT_NAME=clang
+
+if [ $SV_EXTERNALS_VERSION_NUMBER == '2019.02' ]; then
+  export EXTERNALS_BUILD_DATE=2019.11.16
+  export EXTERNALS_SV_FULL_OS_VER_NO=10.11
+  export EXTERNALS_SV_COMPILER_VER_NO=7.3
+  export EXTERNALS_SV_LOWERCASE_CMAKE_BUILD_TYPE=release
+elif [ $SV_EXTERNALS_VERSION_NUMBER == '2019.06' ]; then
+  export EXTERNALS_BUILD_DATE=2019.11.16
+  export EXTERNALS_SV_FULL_OS_VER_NO=10.13
+  export EXTERNALS_SV_COMPILER_VER_NO=8.1
+  export EXTERNALS_SV_LOWERCASE_CMAKE_BUILD_TYPE=release
+fi
+
+export EXTERNALS_TOP=`pwd`/ext
+
+export PARENT_URL=http://simvascular.stanford.edu/downloads/public/svsolver/externals/$SV_EXTERNALS_VERSION_NUMBER/darwin/$EXTERNALS_SV_OS_LONG_NAME_DIR/$EXTERNALS_SV_FULL_OS_VER_NO/$EXTERNALS_SV_COMPILER_SHORT_NAME/$EXTERNALS_SV_COMPILER_VER_NO/$EXTERNALS_SV_ARCH_DIR/$EXTERNALS_SV_LOWERCASE_CMAKE_BUILD_TYPE/$EXTERNALS_BUILD_DATE
+
+export TAR_FILE_PREFIX=$EXTERNALS_SV_OS_LONG_NAME_DIR.$EXTERNALS_SV_FULL_OS_VER_NO.$EXTERNALS_SV_COMPILER_SHORT_NAME.$EXTERNALS_SV_COMPILER_VER_NO.$EXTERNALS_SV_ARCH_DIR.$EXTERNALS_SV_LOWERCASE_CMAKE_BUILD_TYPE.$EXTERNALS_BUILD_DATE
+
+# default is windows, so override with gnu compilers on mac_osx
+
+echo "CLUSTER=x64_macosx" > cluster_overrides.mk
+echo "CXX_COMPILER_VERSION=clang" >> cluster_overrides.mk
+echo "FORTRAN_COMPILER_VERSION=ifort" >> cluster_overrides.mk
+echo "SV_COMPILER=clang" >> cluster_overrides.mk
+echo "SV_COMPILER_VERSION=$EXTERNALS_SV_COMPILER_VER_NO" >> cluster_overrides.mk
+
+echo "SV_EXTERNALS_VERSION_NUMBER=$SV_EXTERNALS_VERSION_NUMBER" >> global_overrides.mk
+echo "OPEN_SOFTWARE_BINARIES_TOPLEVEL=$EXTERNALS_TOP/bin/$EXTERNALS_SV_COMPILER_SHORT_NAME/$EXTERNALS_SV_COMPILER_VER_NO/$EXTERNALS_SV_ARCH_DIR/$EXTERNALS_SV_LOWERCASE_CMAKE_BUILD_TYPE" > global_overrides.mk
+
+mkdir -p $EXTERNALS_TOP
+chmod -R a+rwx $EXTERNALS_TOP
+mkdir -p $EXTERNALS_TOP/tarfiles
+mkdir -p $EXTERNALS_TOP/bin/$EXTERNALS_SV_COMPILER_SHORT_NAME/$EXTERNALS_SV_COMPILER_VER_NO/$EXTERNALS_SV_ARCH_DIR/$EXTERNALS_SV_LOWERCASE_CMAKE_BUILD_TYPE
+
+pushd $EXTERNALS_TOP/tarfiles
+
+if [ $SV_EXTERNALS_VERSION_NUMBER == '2019.02' ]; then
+  wget $PARENT_URL/$TAR_FILE_PREFIX.vtk.8.1.1.tar.gz
+elif [ $SV_EXTERNALS_VERSION_NUMBER == '2019.06' ]; then
+  wget $PARENT_URL/$TAR_FILE_PREFIX.vtk.8.1.1.tar.gz
+fi
+
+popd
+
+pushd $EXTERNALS_TOP/bin/$EXTERNALS_SV_COMPILER_SHORT_NAME/$EXTERNALS_SV_COMPILER_VER_NO/$EXTERNALS_SV_ARCH_DIR/$EXTERNALS_SV_LOWERCASE_CMAKE_BUILD_TYPE
+for i in $EXTERNALS_TOP/tarfiles/$TAR_FILE_PREFIX.*.tar.gz; do
+    echo "untar $i"
+    tar xzf $i
+done
+popd
+
+make fast
+
---
+++ BuildWithMake/quick-build-windows.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Copyright (c) Stanford University, The Regents of the University of
+#               California, and others.
+#
+# All Rights Reserved.
+#
+# See Copyright-SimVascular.txt for additional details.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject
+# to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+# OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#export SV_EXTERNALS_VERSION_NUMBER=2019.02
+export SV_EXTERNALS_VERSION_NUMBER=2019.06
+
+if [ $SV_EXTERNALS_VERSION_NUMBER == '2019.02' ]; then
+  export SV_COMPILER_VERSION=19.0
+  export CXX_COMPILER_VERSION=msvc-19.0
+  export PARENT_URL=http://simvascular.stanford.edu/downloads/public/svsolver/externals/2019.02/windows/windows/10.0/msvc/19.0/x64/release/2019.11.16
+  export TAR_FILE_PREFIX=windows.10.0.msvc.19.0.x64.release.2019.11.16
+elif [ $SV_EXTERNALS_VERSION_NUMBER == '2019.06' ]; then
+  export SV_COMPILER_VERSION=19.16
+  export CXX_COMPILER_VERSION=msvc-19.16
+  # compiler must now be present on the cmd line
+  export PARENT_URL=http://simvascular.stanford.edu/downloads/public/svsolver/externals/2019.06/windows/windows/10.0/msvc/19.16/x64/release/2019.07.05
+  export TAR_FILE_PREFIX=windows.10.0.msvc.19.16.x64.release.2019.11.16
+fi
+
+export EXTERNALS_TOP=`pwd`/ext
+
+
+# default is windows
+
+echo "OPEN_SOFTWARE_BINARIES_TOPLEVEL=`cygpath -m $EXTERNALS_TOP/bin`" >> global_overrides.mk
+echo "SV_EXTERNALS_PREBUILT_QT=0" >> global_overrides.mk
+echo "SV_EXTERNALS_VERSION_NUMBER=$SV_EXTERNALS_VERSION_NUMBER" >> global_overrides.mk
+echo "SV_COMPILER_VERSION=$SV_COMPILER_VERSION" >> global_overrides.mk
+echo "CXX_COMPILER_VERSION=$CXX_COMPILER_VERSION" >> global_overrides.mk
+
+mkdir -p $EXTERNALS_TOP
+chmod -R a+rwx $EXTERNALS_TOP
+mkdir -p $EXTERNALS_TOP/tarfiles
+mkdir -p $EXTERNALS_TOP/bin
+
+pushd $EXTERNALS_TOP/tarfiles
+
+if [ $SV_EXTERNALS_VERSION_NUMBER == '2019.02' ]; then
+  wget $PARENT_URL/$TAR_FILE_PREFIX.vtk.8.1.1.tar.gz
+elif [ $SV_EXTERNALS_VERSION_NUMBER == '2019.06' ]; then
+  wget $PARENT_URL/$TAR_FILE_PREFIX.vtk.8.1.1.tar.gz
+fi
+
+popd
+
+pushd $EXTERNALS_TOP/bin
+for i in $EXTERNALS_TOP/tarfiles/$TAR_FILE_PREFIX.*.tar.gz; do
+    echo "untar $i"
+    tar xzf $i
+done
+popd
+
+make fast
