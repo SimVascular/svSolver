b1e4b8bc62b86634ddaba8da8cd9f0f8565db113
Dave Parker <davep@stanford.edu>
2023-03-20 18:38:59 -0700

    Set test for Fortran compiler to add legacy flag.

--- Code/CMake/SimVascularOptions.cmake
+++ Code/CMake/SimVascularOptions.cmake
@@ -117,7 +117,7 @@ if(CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
   # Set the -std=legacy option to compile code using gcc 8.1 (see svSolver GitHub Issue #32). 
   if(APPLE AND (Fortran_COMPILER_NAME MATCHES "gfortran.*") AND (COMPILER_MAJOR_VERSION GREATER 7))
       set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -std=legacy")
-  elseif(COMPILER_MAJOR_VERSION GREATER 11)
+  elseif(COMPILER_MAJOR_VERSION GREATER 10)
       set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -std=legacy")
   endif()
 else()
